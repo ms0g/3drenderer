ebabd8d5b6013e5e8f3f1629f05e5a806d0d9118
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-01-04 05:48:00 +0300

    refactor(Constant): updated value

--- src/Constants/Constants/Constants.hpp
+++ src/Constants/Constants/Constants.hpp
@@ -2,7 +2,7 @@
 
 #include <cstdint>
 
-constexpr int WINDOW_WIDTH{1280};
-constexpr int WINDOW_HEIGHT{800};
+constexpr int WINDOW_WIDTH{800};
+constexpr int WINDOW_HEIGHT{600};
 
 using color_t = uint32_t;
