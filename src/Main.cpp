c61b17bed957f5eae8311dcbb32f70bc61ef8b15
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-01-22 02:33:05 +0300

    feat(Main): added gui

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,23 @@
 #include "Renderer.h"
+#include "../libs/imgui/imgui_impl_sdl.h"
 
 
 int main() {
-    Renderer renderer{"../assets/f22.obj"};
+    Renderer renderer{"../assets/cube.obj"};
     SDL_Event event;
     bool isRunning{true};
 
     auto ProcessInput = [&]() {
         SDL_PollEvent(&event);
+
+        ImGui_ImplSDL2_ProcessEvent(&event);
+        ImGuiIO& io = ImGui::GetIO();
+
+        int mouseX, mouseY;
+        const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+        io.MousePos = ImVec2(mouseX, mouseY);
+        io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+        io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
 
         switch (event.type) {
             case SDL_QUIT:
