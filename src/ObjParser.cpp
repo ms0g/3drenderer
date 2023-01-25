bc8c096814898eb138938ade66e662d3f42cccca
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-01-25 04:52:20 +0300

    refactor(Objparser): gotten rid of C-style file opening

--- src/ObjParser.cpp
+++ src/ObjParser.cpp
@@ -1,27 +1,31 @@
 #include "ObjParser.h"
+#include <fstream>
 #include "Vec3.h"
 #include "MeshData.hpp"
 
 MeshData ObjParser::Load(const std::string& fileName) {
     MeshData data;
-    FILE* file;
-    file = fopen(fileName.c_str(), "r");
-    char line[1024];
+    std::ifstream file;
 
-    while (fgets(line, 1024, file)) {
+    file.open(fileName.c_str());
+    if (!file.is_open()) return {};
+
+    std::string line;
+
+    while (std::getline(file, line)) {
         // Vertex information
-        if (strncmp(line, "v ", 2) == 0) {
+        if (strncmp(line.c_str(), "v ", 2) == 0) {
             Vec3 vertex;
-            sscanf(line, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+            sscanf(line.c_str(), "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
             data.vertices.push_back(vertex);
         }
         // Face information
-        if (strncmp(line, "f ", 2) == 0) {
+        if (strncmp(line.c_str(), "f ", 2) == 0) {
             int vertex_indices[3];
             int texture_indices[3];
             int normal_indices[3];
             sscanf(
-                    line, "f %d/%d/%d %d/%d/%d %d/%d/%d",
+                    line.c_str(), "f %d/%d/%d %d/%d/%d %d/%d/%d",
                     &vertex_indices[0], &texture_indices[0], &normal_indices[0],
                     &vertex_indices[1], &texture_indices[1], &normal_indices[1],
                     &vertex_indices[2], &texture_indices[2], &normal_indices[2]
@@ -34,5 +38,6 @@ MeshData ObjParser::Load(const std::string& fileName) {
             data.faces.push_back(face);
         }
     }
+    file.close();
     return data;
 }
