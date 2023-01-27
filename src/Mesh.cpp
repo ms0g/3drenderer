251ec1eb3175cfbcd186db5b016a813b91b3dfb5
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-01-27 03:57:03 +0300

    feat(Mesh): added scale and translation unit

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -4,6 +4,8 @@
 
 void Mesh::SetData(MeshData& meshData) {
     m_rotation = {0, 0, 0};
+    m_translation = {0, 0, 0};
+    m_scale = {1.0, 1.0, 1.0};
 
     // Fill vertices
     m_vertices = meshData.vertices;
@@ -13,18 +15,30 @@ void Mesh::SetData(MeshData& meshData) {
 
 }
 
-void Mesh::UpdateRotation(float angle) {
-    UpdateRotationX(angle);
-    UpdateRotationY(angle);
-    UpdateRotationZ(angle);
-}
-
 std::vector<TriangleFace>& Mesh::GetFaces() {
     return m_faces;
 }
 
 std::vector<Vec3>& Mesh::GetVertices() {
     return m_vertices;
+}
+
+void Mesh::UpdateRotation(float angle) {
+    UpdateRotationX(angle);
+    UpdateRotationY(angle);
+    UpdateRotationZ(angle);
+}
+
+void Mesh::UpdateRotationX(float angle) {
+    m_rotation.x += angle;
+}
+
+void Mesh::UpdateRotationY(float angle) {
+    m_rotation.y += angle;
+}
+
+void Mesh::UpdateRotationZ(float angle) {
+    m_rotation.z += angle;
 }
 
 float Mesh::GetRotationX() const {
@@ -39,14 +53,62 @@ float Mesh::GetRotationZ() const {
     return m_rotation.z;
 }
 
-void Mesh::UpdateRotationX(float angle) {
-    m_rotation.x += angle;
+void Mesh::UpdateScale(float scalar) {
+    UpdateScaleX(scalar);
+    UpdateScaleY(scalar);
+    UpdateScaleZ(scalar);
 }
 
-void Mesh::UpdateRotationY(float angle) {
-    m_rotation.y += angle;
+void Mesh::UpdateScaleX(float scalar) {
+    m_scale.x += scalar;
 }
 
-void Mesh::UpdateRotationZ(float angle) {
-    m_rotation.z += angle;
+void Mesh::UpdateScaleY(float scalar) {
+    m_scale.y += scalar;
+}
+
+void Mesh::UpdateScaleZ(float scalar) {
+    m_scale.z += scalar;
+}
+
+float Mesh::GetScaleX() const {
+    return m_scale.x;
+}
+
+float Mesh::GetScaleY() const {
+    return m_scale.y;
+}
+
+float Mesh::GetScaleZ() const {
+    return m_scale.z;
+}
+
+void Mesh::UpdateTranslation(float coord) {
+    UpdateTranslationX(coord);
+    UpdateTranslationY(coord);
+    UpdateTranslationZ(coord);
+}
+
+void Mesh::UpdateTranslationX(float coord) {
+    m_translation.x += coord;
+}
+
+void Mesh::UpdateTranslationY(float coord) {
+    m_translation.y += coord;
+}
+
+void Mesh::UpdateTranslationZ(float coord) {
+    m_translation.z += coord;
+}
+
+float Mesh::GetTranslationX() const {
+    return m_translation.x;
+}
+
+float Mesh::GetTranslationY() const {
+    return m_translation.y;
+}
+
+float Mesh::GetTranslationZ() const {
+    return m_translation.z;
 }
--- src/Mesh.h
+++ src/Mesh.h
@@ -19,19 +19,48 @@ public:
 
     void UpdateRotationZ(float angle);
 
-    [[nodiscard]] std::vector<TriangleFace>& GetFaces();
-
-    [[nodiscard]] std::vector<Vec3>& GetVertices();
-
     [[nodiscard]] float GetRotationX() const;
 
     [[nodiscard]] float GetRotationY() const;
 
     [[nodiscard]] float GetRotationZ() const;
 
+    void UpdateScale(float scalar);
+
+    void UpdateScaleX(float scalar);
+
+    void UpdateScaleY(float scalar);
+
+    void UpdateScaleZ(float scalar);
+
+    [[nodiscard]] float GetScaleX() const;
+
+    [[nodiscard]] float GetScaleY() const;
+
+    [[nodiscard]] float GetScaleZ() const;
+
+    void UpdateTranslation(float coord);
+
+    void UpdateTranslationX(float coord);
+
+    void UpdateTranslationY(float coord);
+
+    void UpdateTranslationZ(float coord);
+
+    [[nodiscard]] float GetTranslationX() const;
+
+    [[nodiscard]] float GetTranslationY() const;
+
+    [[nodiscard]] float GetTranslationZ() const;
+
+    [[nodiscard]] std::vector<TriangleFace>& GetFaces();
+
+    [[nodiscard]] std::vector<Vec3>& GetVertices();
 
 private:
     Vec3 m_rotation;
+    Vec3 m_scale;
+    Vec3 m_translation;
     std::vector<Vec3> m_vertices;
     std::vector<TriangleFace> m_faces;
 };
