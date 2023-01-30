454490d525ab4557f7984b6d67d471db9cd86b39
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-01-31 02:35:07 +0300

    feat(Constant): added constants to be used in perspective matrix

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <cstdint>
+#include <numbers>
 
 constexpr int WINDOW_WIDTH{800};
 constexpr int WINDOW_HEIGHT{600};
+
+constexpr float fov{std::numbers::pi/3.0};
+constexpr float aspect{(float)WINDOW_HEIGHT/(float)WINDOW_WIDTH};
+constexpr float znear{0.1};
+constexpr float zfar{100.0};
 
 using color_t = uint32_t;
