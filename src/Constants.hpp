a45f684a8e355fe3dce132c944a176be63efa7a9
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-03 17:51:08 +0300

    refactor(Constant): changed name

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -6,9 +6,9 @@
 constexpr int WINDOW_WIDTH{800};
 constexpr int WINDOW_HEIGHT{600};
 
-constexpr float fov{std::numbers::pi/3.0};
-constexpr float aspect{(float)WINDOW_HEIGHT/(float)WINDOW_WIDTH};
-constexpr float znear{0.1};
-constexpr float zfar{100.0};
+constexpr float FOV{std::numbers::pi / 3.0};
+constexpr float ASPECT{(float)WINDOW_HEIGHT / (float)WINDOW_WIDTH};
+constexpr float ZNEAR{0.1};
+constexpr float ZFAR{100.0};
 
 using color_t = uint32_t;
