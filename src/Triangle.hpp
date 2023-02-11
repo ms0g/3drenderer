4c1a431c26a85ee595640e7dca2c0257b5b2f04b
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-12 01:58:55 +0300

    feat(Triangle): added texture

--- src/Triangle.hpp
+++ src/Triangle.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "Vec2.h"
+#include "Texture.h"
 
 struct TriangleFace {
-    int a, b, c;
-    uint32_t color;
+    int a{}, b{}, c{};
+    Texture a_uv, b_uv, c_uv;
+    uint32_t color{};
 };
 
 struct Triangle {
     Vec2 points[3];
+    Texture texcoords[3];
     float avgDepth;
     uint32_t color;
 };
