48b351af45441c40f53429d1b1b399436c9abdb1
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-19 02:34:33 +0300

    feat(Obj): added texture coord parsing routine

--- src/ObjParser.cpp
+++ src/ObjParser.cpp
@@ -11,6 +11,7 @@ MeshData ObjParser::Load(const std::string& fileName) {
     if (!file.is_open()) return {};
 
     std::string line;
+    std::vector<Texture> texcoords;
 
     while (std::getline(file, line)) {
         // Vertex information
@@ -19,6 +20,14 @@ MeshData ObjParser::Load(const std::string& fileName) {
             sscanf(line.c_str(), "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
             data.vertices.push_back(vertex);
         }
+
+        // Texture coordinate information
+        if (strncmp(line.c_str(), "vt ", 3) == 0) {
+            Texture texcoord;
+            sscanf(line.c_str(), "vt %f %f", &texcoord.u, &texcoord.v);
+            texcoords.push_back(texcoord);
+        }
+
         // Face information
         if (strncmp(line.c_str(), "f ", 2) == 0) {
             int vertex_indices[3];
@@ -31,12 +40,12 @@ MeshData ObjParser::Load(const std::string& fileName) {
                     &vertex_indices[2], &texture_indices[2], &normal_indices[2]
             );
             TriangleFace face = {
-                    vertex_indices[0],
-                    vertex_indices[1],
-                    vertex_indices[2],
-                    Texture(),
-                    Texture(),
-                    Texture(),
+                    vertex_indices[0] - 1,
+                    vertex_indices[1] - 1,
+                    vertex_indices[2] - 1,
+                    texcoords.at(texture_indices[0] - 1),
+                    texcoords.at(texture_indices[1] - 1),
+                    texcoords.at(texture_indices[2] - 1),
                     0xFFFFFFFF
             };
             data.faces.push_back(face);
