e96e2f463f5168aa3e4674cd0a6ff89ffff4370d
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-20 19:28:30 +0300

    fix(App): minor

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,9 +5,9 @@ int main() {
     App app{};
 
     app.LoadMesh("../assets/runway.obj", "../assets/runway.png", {1, 1, 1}, {0, -1.5, +23}, {0, 0, 0});
-    app.LoadMesh("../assets/f22.obj", "../assets/f22.png", {1, 1, 1}, {0, -1.3, +5}, {0, -M_PI/2, 0});
-    app.LoadMesh("../assets/efa.obj", "../assets/efa.png", {1, 1, 1}, {-2, -1.3, +9}, {0, -M_PI/2, 0});
-    app.LoadMesh("../assets/f117.obj", "../assets/f117.png", {1, 1, 1}, {+2, -1.3, +9}, {0, -M_PI/2, 0});
+    app.LoadMesh("../assets/f22.obj", "../assets/f22.png", {1, 1, 1}, {0, -1.3, +5}, {0, -std::numbers::pi/2, 0});
+    app.LoadMesh("../assets/efa.obj", "../assets/efa.png", {1, 1, 1}, {-2, -1.3, +9}, {0, -std::numbers::pi/2, 0});
+    app.LoadMesh("../assets/f117.obj", "../assets/f117.png", {1, 1, 1}, {+2, -1.3, +9}, {0, -std::numbers::pi/2, 0});
 
     while (app.IsRunning()) {
         app.Input();
