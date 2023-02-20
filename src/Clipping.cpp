c02b5f4d825fb491db9a4ef204b6b2e435bd1850
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-20 06:59:05 +0300

    feat(Clipping): added uv interpolation

--- src/Clipping.cpp
+++ src/Clipping.cpp
@@ -72,17 +72,22 @@ void Frustum::StartClipping(polygon_t& p) {
     ClipPolygonAgainstPlane(p, FRUSTUM::FAR_PLANE);
 }
 
+
 void Frustum::ClipPolygonAgainstPlane(polygon_t& p, int plane) {
     vec3 planePoint = m_planes[plane].point;
     vec3 planeNormal = m_planes[plane].normal;
 
     // Declare a static array of inside vertices that will be part of the final polygon returned via parameter
     vec3 insideVertices[MAX_NUM_POLY_VERTICES];
+    texcoord insideTexcoords[MAX_NUM_POLY_VERTICES];
     int numInsideVertices = 0;
 
-    // Start current and previous vertex with the first and last polygon vertices
+    // Start current vertex and texcoord with the first ones
     vec3* currentVertex = &p.vertices[0];
+    texcoord* currentTexcoord = &p.texcoords[0];
+    //
     vec3* previousVertex = &p.vertices[p.numVertices - 1];
+    texcoord* previousTexcoord = &p.texcoords[p.numVertices - 1];
 
     // Calculate the dot product of the current and previous vertex
     float currentDot;
@@ -98,37 +103,47 @@ void Frustum::ClipPolygonAgainstPlane(polygon_t& p, int plane) {
             float t = previousDot / (previousDot - currentDot);
 
             // Calculate the intersection point I = Q1 + t(Q2-Q1)
-            vec3 intersectionPoint = *currentVertex;    // I =        Qc
-            intersectionPoint -= *previousVertex;      // I =       (Qc-Qp)
-            intersectionPoint *= t;                     // I =      t(Qc-Qp)
-            intersectionPoint += *previousVertex;       // I = Qp + t(Qc-Qp)
+            vec3 intersectionPoint = {
+                    utils::lerp(previousVertex->x, currentVertex->x, t),
+                    utils::lerp(previousVertex->y, currentVertex->y, t),
+                    utils::lerp(previousVertex->z, currentVertex->z, t)};
+
+            texcoord intersectionTexcoord = {
+                    utils::lerp(previousTexcoord->u, currentTexcoord->u, t),
+                    utils::lerp(previousTexcoord->v, currentTexcoord->v, t)
+            };
 
             // Insert the intersection point to the list of "inside vertices"
+            insideTexcoords[numInsideVertices] = intersectionTexcoord;
             insideVertices[numInsideVertices++] = intersectionPoint;
 
         }
         // Current vertex is inside the plane
         if (currentDot > 0) {
+            insideTexcoords[numInsideVertices] = *currentTexcoord;
             insideVertices[numInsideVertices++] = *currentVertex;
         }
 
         // Move to the next vertex
         previousDot = currentDot;
         previousVertex = currentVertex;
+        previousTexcoord = currentTexcoord;
         ++currentVertex;
+        ++currentTexcoord;
     }
 
     // At the end, copy the list of inside vertices into the destination polygon (out parameter)
     for (int i = 0; i < numInsideVertices; i++) {
         p.vertices[i] = insideVertices[i];
+        p.texcoords[i] = insideTexcoords[i];
     }
     p.numVertices = numInsideVertices;
 
 }
 
 
-polygon_t polygon_t::FromTriangle(vec3 v0, vec3 v1, vec3 v2) {
-    return {{v0, v1, v2}, 3};
+polygon_t polygon_t::FromTriangle(vec3 v0, vec3 v1, vec3 v2, texcoord t0, texcoord t1, texcoord t2) {
+    return {{v0, v1, v2}, {t0, t1, t2}, 3};
 }
 
 
@@ -141,6 +156,14 @@ void polygon_t::TrianglesFromPolygon(polygon_t& p, Triangle* triangles, int& num
         triangles[i].points[0] = vec4::FromVec3(p.vertices[index0]);
         triangles[i].points[1] = vec4::FromVec3(p.vertices[index1]);
         triangles[i].points[2] = vec4::FromVec3(p.vertices[index2]);
+
+        triangles[i].texcoords[0] = p.texcoords[index0];
+        triangles[i].texcoords[1] = p.texcoords[index1];
+        triangles[i].texcoords[2] = p.texcoords[index2];
     }
     numTriangles = p.numVertices - 2;
+}
+
+float utils::lerp(float a, float b, float t) {
+    return a + t * (b - a);
 }
--- src/Clipping.h
+++ src/Clipping.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vec3.h"
+#include "Texture.h"
 
 #define MAX_NUM_POLY_VERTICES 10
 #define MAX_NUM_POLY_TRIANGLES MAX_NUM_POLY_VERTICES
@@ -22,9 +23,10 @@ struct plane_t {
 class Triangle;
 struct polygon_t {
     vec3 vertices[MAX_NUM_POLY_VERTICES];
+    texcoord texcoords[MAX_NUM_POLY_VERTICES];
     int numVertices;
 
-    static polygon_t FromTriangle(vec3 v0, vec3 v1, vec3 v2);
+    static polygon_t FromTriangle(vec3 v0, vec3 v1, vec3 v2, texcoord t0, texcoord t1, texcoord t2);
 
     static void TrianglesFromPolygon(polygon_t& p, Triangle* triangles, int& numTriangles);
 };
@@ -42,3 +44,7 @@ private:
     static constexpr int NUM_PLANES = 6;
     plane_t m_planes[NUM_PLANES];
 };
+
+namespace utils {
+float lerp(float a, float b, float t);
+}
