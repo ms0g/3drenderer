6531ec64d7db811c535312cc65ec9daf1664210a
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-20 05:34:06 +0300

    fix(Culling): fixed horizontal aspect problem

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -6,9 +6,14 @@
 constexpr int WINDOW_WIDTH{800};
 constexpr int WINDOW_HEIGHT{600};
 
-constexpr float FOV{std::numbers::pi / 3.0};
-constexpr float ASPECT{(float)WINDOW_HEIGHT / (float)WINDOW_WIDTH};
-constexpr float ZNEAR{0.1};
-constexpr float ZFAR{100.0};
+constexpr float ASPECT_Y{(float) WINDOW_HEIGHT / (float) WINDOW_WIDTH};
+constexpr float ASPECT_X{(float) WINDOW_WIDTH / (float) WINDOW_HEIGHT};
+
+constexpr float FOV_Y{std::numbers::pi / 3.0};
+const float FOV_X{2 * atan(tan(FOV_Y / 2) * ASPECT_X)};
+
+
+constexpr float ZNEAR{1.0};
+constexpr float ZFAR{20.0};
 
 using color_t = uint32_t;
