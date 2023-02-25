0bb98ab13be8170f3cb17f929dabe1c0d6732a8e
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2023-02-25 03:04:32 +0300

    refactor(Triangle): one line computation of normals

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -7,14 +7,11 @@ vec3 ComputeTriangleNormal(vec4* vertices) {
     vec3 vecC = vec3::FromVec4(vertices[2]);     /* C-----B */
 
     // Get the vector subtraction of B-A and C-A
-    vec3 ab = vecB - vecA;
-    vec3 ac = vecC - vecA;
-    ab.Normalize();
-    ac.Normalize();
+    vec3 ab = (vecB - vecA).Normalize();
+    vec3 ac = (vecC - vecA).Normalize();
 
     // Compute the face normal
-    vec3 normal = ab.Cross(ac);
-    normal.Normalize();
+    vec3 normal = ab.Cross(ac).Normalize();
 
     return normal;
 }
